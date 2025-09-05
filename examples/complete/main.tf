0d454b5e89042fff22a6735a1a3f04bcc4a89282
Federico Midaglia <fmidaglia@gocloud.la>
2025-09-05 13:33:43 -0300

    feat(module): initial release (#1)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: gdacal.gocloud <gdacal@gocloud.la>
    Co-authored-by: Elias Lischinsky <elischinsky@gocloud.la>
    Co-authored-by: Gonzalo Dacal <146208139+gdacal-gocloud@users.noreply.github.com>

---
+++ examples/complete/main.tf
@@ -0,0 +1,21 @@
+module "wrapper_cloudmap" {
+  source = "../../"
+
+  metadata = local.metadata
+
+  cloudmap_parameters = {
+    "project1.${local.zone_internal}" = {
+      # Optional: Override VPC ID for this specific namespace
+      # vpc_id = "vpc-specific-for-project1"
+    }
+    "project2.${local.zone_internal}" = {
+      # Optional: Override VPC ID for this specific namespace
+      # vpc_id = "vpc-specific-for-project2"
+    }
+  }
+
+  # It should come as output from the wrapper_vpc module.
+  # vpc_id = module.wrapper_vpc.vpc.vpc_id
+
+  vpc_id = "vpc-xxxxxxxxxxxxxx"
+}
---
+++ variables.tf
@@ -0,0 +1,28 @@
+/*----------------------------------------------------------------------*/
+/* Common |                                                             */
+/*----------------------------------------------------------------------*/
+
+variable "metadata" {
+  type = any
+}
+
+/*----------------------------------------------------------------------*/
+/* CloudMap | Variable Definition                                       */
+/*----------------------------------------------------------------------*/
+variable "cloudmap_parameters" {
+  type        = any
+  description = "CloudMap parameteres to declare Namespaces"
+  default     = {}
+}
+
+variable "cloudmap_defaults" {
+  type        = any
+  description = "CloudMap default parameteres to declare Namespaces"
+  default     = {}
+}
+
+variable "vpc_id" {
+  type        = string
+  description = ""
+  default     = ""
+}
